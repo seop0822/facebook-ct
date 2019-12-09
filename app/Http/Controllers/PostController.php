6343679cfdf0a146f0304a95031570dd215f8853
unknown <seop0822@naver.com>
2019-12-10 01:05:21 +0900

    Asserting on the Response

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function store()
+    {
+        $data = request()->validate([
+           'data.attributes.body' => '',
+        ]);
+
+        $post = request()->user()->posts()->create($data['data']['attributes']);
+
+        return response([
+            'data' => [
+                'type' => 'posts',
+                'post_id' => $post->id,
+                'attributes' => [
+                    'body' => $post->body,
+                ]
+            ],
+            'links' => [
+                'self' => url('posts/' . $post->id)
+            ]
+        ], 201);
+    }
+}
