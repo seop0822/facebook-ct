8640efdd6e442b5c44acbef00b4ac0e17eaca997
unknown <seop0822@naver.com>
2020-02-26 19:21:24 +0900

    User can Post Comments Test

--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,12 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->group(function () {
 
-//   Route::get('/user', function (Request $request) {
-//        return $request->user();
-//    });
-//
-//   Route::get('/posts', 'PostController@index');
-//   Route::post('/posts', 'PostController@store');
-
-
     Route::get('auth-user', 'AuthUserController@show');
 
     Route::apiResources([
         'posts' => 'PostController',
         'posts/{post}/like' => 'PostLikeController',
+        'posts/{post}/comment' => 'PostCommentController',
         'users' => 'UserController',
         'users/{user}/posts' => 'UserPostController',
         'friend-request' => 'FriendRequestController',
